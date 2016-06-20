ad9d08cc82b1788c48ffd272324f3f8666197646
Rodrigo Bernardi <coloradonanet@hotmai.com>
2016-06-19 22:59:10 -0300

    Generic

--- ScrollTable/src/main/java/com/loopeer/android/librarys/scrolltable/IScroller.java
+++
@@ -1,7 +0,0 @@
-package com.loopeer.android.librarys.scrolltable;
-
-public interface IScroller {
-
-    void onScrollXY(int offsetX, int offsetY);
-
-}
--- app/src/main/java/com/github/snakeice/realmdroid/enums/FieldTypes.java
+++
@@ -1,8 +0,0 @@
-package com.github.snakeice.realmdroid.enums;
-
-/**
- * Created by Rodrigo on 12/05/2016.
- */
-public enum FieldTypes {
-    INTEGER, LONG, STRING, DATE, BOOLEAN
-}
